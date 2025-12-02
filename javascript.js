148626368a9e9898726e96b84ac95c0789741acf
ericgf78 <ericgf78@gmail.com>
2025-12-02 17:32:37 -0500

    add some UI and logic
    Added some assets and created the logic to select the tool.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,68 @@
-let canvasContainer = document.querySelector("#innerContainer");
-let slider = document.querySelector('#slider');
+const canvasContainer = document.querySelector("#innerContainer");
+const slider = document.querySelector('#slider');
+const pencil = document.querySelector("#pencil");
+const brush = document.querySelector("#brush");
+const unicorn = document.querySelector("#unicorn");
+let pencilOn = true;
+let brushOn = false;
+let unicornOn = false;
 let sliderValue = slider.value;
 let gridSize = 10;
 
-function createGridDiv(divSize) {
-    const gridDiv = document.createElement("div");
-    const itemSize = 100 / divSize;
-    gridDiv.classList.add("gridDiv");
-    gridDiv.style.flexBasis = `${itemSize}%`;
-    canvasContainer.appendChild(gridDiv);
-    
-}
 
 function generateGrid(size) { 
-    sliderDisplay.innerHTML = `Size: ${slider.value}`;
+    sliderDisplay.innerHTML = `Size: ${slider.value} X ${slider.value}` ;
     canvasContainer.innerHTML = '';
-    for (let i = 0; i < size * size; i++) {
-        createGridDiv(size);
+   
+    if (pencilOn == true) {
+        for (let i = 0; i < size * size; i++) {
+            brushOn = false;
+            unicornOn = false;
+            const gridDiv = document.createElement("div");
+            const itemSize = 100 / size;
+            gridDiv.classList.add("gridDiv");
+            gridDiv.style.flexBasis = `${itemSize}%`;
+            canvasContainer.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(112, 112, 112, 1)';});
+        }
+    }    
+    if (brushOn == true) {    
+        for (let i = 0; i < size * size; i++) {
+            pencilOn = false;
+            unicornOn = false;
+            const gridDiv = document.createElement("div");
+            const itemSize = 100 / size;
+            gridDiv.classList.add("gridDiv");
+            gridDiv.style.flexBasis = `${itemSize}%`;
+            canvasContainer.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(25, 220, 255, 1)';});
+        }        
        
-    }     
+    }
+    if (unicornOn == true) {    
+        for (let i = 0; i < size * size; i++) {
+            pencilOn = false;
+            brushOn = false;
+            const gridDiv = document.createElement("div");
+            const itemSize = 100 / size;
+            gridDiv.classList.add("gridDiv");
+            gridDiv.style.flexBasis = `${itemSize}%`;
+            canvasContainer.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(25, 220, 255, 1)';});
+        }        
+       
+    }         
 }
 
+
+
+
 generateGrid(sliderValue);
+pencil.addEventListener("click", () => {pencilOn = true; brushOn = false; unicornOn = false; generateGrid(sliderValue);});
+brush.addEventListener("click", () => {brushOn = true; pencilOn = false; unicornOn = false; generateGrid(sliderValue);});
+unicorn.addEventListener("click", () => {unicornOn = true; brushOn = false; pencilOn = false; generateGrid(sliderValue);});
+
+
+
+
 
