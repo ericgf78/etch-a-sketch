3b4f2ea5227f014a45383ecc738465295d96acbe
ericgf78 <ericgf78@gmail.com>
2025-12-02 19:17:02 -0500

    create brush and color picker logic
    Created the logic to select a color for the brush tool.

--- javascript.js
+++ javascript.js
@@ -1,68 +1,49 @@
 const canvasContainer = document.querySelector("#innerContainer");
-const slider = document.querySelector('#slider');
+const slider = document.querySelector("#slider");
 const pencil = document.querySelector("#pencil");
 const brush = document.querySelector("#brush");
 const unicorn = document.querySelector("#unicorn");
+const colorInput = document.querySelector("#colorInput")
+let selectedColor = "#6a97d2";
+let currentColor = "rgba(112, 112, 112, 1)";
 let pencilOn = true;
 let brushOn = false;
 let unicornOn = false;
 let sliderValue = slider.value;
 let gridSize = 10;
 
-
-function generateGrid(size) { 
-    sliderDisplay.innerHTML = `Size: ${slider.value} X ${slider.value}` ;
-    canvasContainer.innerHTML = '';
-   
-    if (pencilOn == true) {
-        for (let i = 0; i < size * size; i++) {
-            brushOn = false;
-            unicornOn = false;
-            const gridDiv = document.createElement("div");
-            const itemSize = 100 / size;
-            gridDiv.classList.add("gridDiv");
-            gridDiv.style.flexBasis = `${itemSize}%`;
-            canvasContainer.appendChild(gridDiv);
-            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(112, 112, 112, 1)';});
-        }
-    }    
-    if (brushOn == true) {    
-        for (let i = 0; i < size * size; i++) {
-            pencilOn = false;
-            unicornOn = false;
-            const gridDiv = document.createElement("div");
-            const itemSize = 100 / size;
-            gridDiv.classList.add("gridDiv");
-            gridDiv.style.flexBasis = `${itemSize}%`;
-            canvasContainer.appendChild(gridDiv);
-            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(25, 220, 255, 1)';});
-        }        
-       
-    }
-    if (unicornOn == true) {    
-        for (let i = 0; i < size * size; i++) {
-            pencilOn = false;
-            brushOn = false;
-            const gridDiv = document.createElement("div");
-            const itemSize = 100 / size;
-            gridDiv.classList.add("gridDiv");
-            gridDiv.style.flexBasis = `${itemSize}%`;
-            canvasContainer.appendChild(gridDiv);
-            gridDiv.addEventListener('mouseover', function(e) {e.target.style.background = 'rgba(25, 220, 255, 1)';});
-        }        
-       
-    }         
+function selectColor(color) {
+    selectedColor = color;
+    currentColor = selectedColor;
 }
 
-
-
+function generateGrid(size) {
+  sliderDisplay.innerHTML = `Size: ${slider.value} X ${slider.value}`;
+  canvasContainer.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+      brushOn = false;
+      unicornOn = false;
+      const gridDiv = document.createElement("div");
+      const itemSize = 100 / size;
+      gridDiv.classList.add("gridDiv");
+      gridDiv.style.flexBasis = `${itemSize}%`;
+      canvasContainer.appendChild(gridDiv);
+      gridDiv.addEventListener("mouseover", function (e) {
+        e.target.style.background = currentColor;
+      });
+    }
+}
 
 generateGrid(sliderValue);
-pencil.addEventListener("click", () => {pencilOn = true; brushOn = false; unicornOn = false; generateGrid(sliderValue);});
-brush.addEventListener("click", () => {brushOn = true; pencilOn = false; unicornOn = false; generateGrid(sliderValue);});
-unicorn.addEventListener("click", () => {unicornOn = true; brushOn = false; pencilOn = false; generateGrid(sliderValue);});
 
-
-
-
-
+pencil.addEventListener("click", () => {
+    currentColor = "rgba(112, 112, 112, 1)";
+});
+brush.addEventListener("click", () => {
+    console.log(selectedColor);
+    currentColor = selectedColor;
+    colorInput.click()
+});
+unicorn.addEventListener("click", () => {
+    currentColor = "rgba(112, 112, 112, 1)";
+});
