8fbc47e334b596217c54015072db49acfd20ec0a
ericgf78 <ericgf78@gmail.com>
2025-12-02 23:29:34 -0500

    feat: shading logic

--- javascript.js
+++ javascript.js
@@ -5,45 +5,66 @@ const brush = document.querySelector("#brush");
 const unicorn = document.querySelector("#unicorn");
 const colorInput = document.querySelector("#colorInput")
 let selectedColor = "#6a97d2";
-let currentColor = "rgba(112, 112, 112, 1)";
-let pencilOn = true;
-let brushOn = false;
-let unicornOn = false;
+let currentColor = "rgba(0, 0, 0, 1)";
 let sliderValue = slider.value;
 let gridSize = 10;
+let unicornOn = false;
+
+function getRandomHexColor() {
+  // Generate a random number between 0 and 16777215 (FFFFFF in hexadecimal)
+  const randomNum = Math.floor(Math.random() * 16777215); 
+  // Convert the number to a hexadecimal string
+  let hexColor = randomNum.toString(16);
+  // Pad the string with leading zeros if necessary to ensure 6 characters
+  hexColor = hexColor.padStart(6, '0'); 
+  return `#${hexColor}`;
+}
+
 
 function selectColor(color) {
     selectedColor = color;
     currentColor = selectedColor;
 }
 
+
+
 function generateGrid(size) {
   sliderDisplay.innerHTML = `Size: ${slider.value} X ${slider.value}`;
   canvasContainer.innerHTML = "";
     for (let i = 0; i < size * size; i++) {
-      brushOn = false;
-      unicornOn = false;
       const gridDiv = document.createElement("div");
       const itemSize = 100 / size;
       gridDiv.classList.add("gridDiv");
       gridDiv.style.flexBasis = `${itemSize}%`;
       canvasContainer.appendChild(gridDiv);
       gridDiv.addEventListener("mouseover", function (e) {
-        e.target.style.background = currentColor;
+        if (unicornOn == true) {
+             e.target.style.background = getRandomHexColor();  
+               
+        }
+        else {
+            let newOpacity = Number(e.target.style.opacity) + 0.1; 
+            e.target.style.background = currentColor;
+            e.target.style.opacity = Math.min(newOpacity, 1);
+            console.log(e.target.style.opacity);
+        }
       });
     }
 }
 
+
 generateGrid(sliderValue);
 
 pencil.addEventListener("click", () => {
-    currentColor = "rgba(112, 112, 112, 1)";
+    unicornOn = false;
+    currentColor = "rgba(0, 0, 0, 1)";
 });
 brush.addEventListener("click", () => {
-    console.log(selectedColor);
+    
+    unicornOn = false;
     currentColor = selectedColor;
     colorInput.click()
 });
 unicorn.addEventListener("click", () => {
-    currentColor = "rgba(112, 112, 112, 1)";
+    unicornOn = true;
 });
